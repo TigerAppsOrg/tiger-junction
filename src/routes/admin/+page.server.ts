f85b062895c6fce46b82760905c05d444ddfe5b1
joshuamotoaki <dachshunduniverse@gmail.com>
2023-08-01 07:48:50 +0200

    Update admin logic

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -7,8 +7,16 @@ import { getAllCourseEvaluations, getCourseData, getCourseEvaluation, getCourseI
 // Only allow admins to access this page
 export const load = async ({ locals }) => {
     let session = await locals.getSession();
-    if (session?.user.id !== ADMIN_ID) throw redirect(303, "/");
     if (!session) throw redirect(303, "/");
+
+    let { data, error } = await locals.supabase
+        .from("private_profiles")
+        .select("is_admin")
+        .eq("id", session.user.id);
+    
+    if (error) throw new Error(error.message);
+    if (!data) throw new Error("No data found");
+    if (!data[0].is_admin) throw redirect(303, "/");
 }
 
 export const actions: Actions = {
