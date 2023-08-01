49232151fc0bedbb630b045c539a139669f06414
joshuamotoaki <dachshunduniverse@gmail.com>
2023-08-01 07:53:29 +0200

    Add logout button to playground

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+import { goto } from "$app/navigation";
 import SideNav from "$lib/components/SideNav.svelte";
 import Card from "$lib/components/elements/Card.svelte";
 import { darkTheme } from "$lib/stores/state";
 
+export let data;
 $: dark = $darkTheme;
+
+const handleLogout = async () => { 
+    const { error } = await data.supabase.auth.signOut();
+    if (!error) goto("/");
+}
 </script>
 
 <div class="bg h-screen w-screen" class:dark>
     <div class="display h-screen w-screen">
         <SideNav />
+        <button on:click={handleLogout} class="p-2 bg-red-400">
+            Logout
+        </button>
         <main>
             <Card>Hello, World!</Card>
         </main>
