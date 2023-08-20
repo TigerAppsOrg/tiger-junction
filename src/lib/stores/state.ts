b5bec243a2268980600ad994ef295fc2ae0426fc
Joshua Lau <dachshunduniverse@gmail.com>
2023-08-20 08:21:07 -0700

    Begin work on darkmode switch

--- src/lib/stores/state.ts
+++ src/lib/stores/state.ts
@@ -2,4 +2,4 @@ import { writable } from "svelte/store";
 
 export const currentPage = writable("home");
 
-export const darkTheme = writable(true);
+export const darkTheme = writable(false);
--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 import { goto } from "$app/navigation";
-import SideNav from "$lib/components/SideNav.svelte";
-import Card from "$lib/components/elements/Card.svelte";
-import { darkTheme } from "$lib/stores/state";
+    import LightSwitch from "$lib/components/general/LightSwitch.svelte";
 
 export let data;
-$: dark = $darkTheme;
 
 const handleLogout = async () => { 
     const { error } = await data.supabase.auth.signOut();
@@ -13,25 +10,11 @@ const handleLogout = async () => {
 }
 </script>
 
-<div class="bg h-screen w-screen" class:dark>
-    <div class="display h-screen w-screen">
-        <SideNav />
-        <button on:click={handleLogout} class="p-2 bg-red-400">
-            Logout
-        </button>
-        <main>
-            <Card>Hello, World!</Card>
-        </main>
-    </div>
+<div class=" h-screen w-screen bg-white dark:bg-synth-dark">
+    <LightSwitch />
 </div>
 
 <style lang="postcss">
-    .bg {
-        @apply bg-gradient-to-br from-blue-500 to-purple-500
-    }
 
-    .display {
-        @apply backdrop-blur-md bg-white/90 dark:bg-black/90;
-    }
 
 </style>
