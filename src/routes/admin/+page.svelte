3f0114cab43bf9a8527a10e1d452f7d3c173bd57
Joshua Lau <dachshunduniverse@gmail.com>
2023-08-20 16:38:10 -0700

    Add home link in admin dashboard

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import { enhance } from "$app/forms";
 import { goto } from "$app/navigation";
-    import LightButton from "$lib/components/general/LightButton.svelte";
+import LightButton from "$lib/components/general/LightButton.svelte";
+import homeIcon from "$lib/img/icons/homeicon.svg";
 import { TERM_MAP } from "$lib/constants.js";
 
 export let data;
@@ -50,7 +51,18 @@ const handleLogout = async () => {
             <h1 class="text-xl">TigerJunction Admin Dashboard</h1>
         </div>
         <div class="flex gap-4">
-            <button class="hover:text-gray-200 duration-150 text-xl
+            <a href="/home" class="flex justify-center items-center 
+            hover:text-slate-600
+            gap-1 text-xl duration-150 dark:hover:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>              
+                Home
+            </a>
+            <button class="dark:hover:text-gray-200
+            hover:text-slate-600
+            duration-150 text-xl
             flex items-center gap-1"
             on:click={handleLogout}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
