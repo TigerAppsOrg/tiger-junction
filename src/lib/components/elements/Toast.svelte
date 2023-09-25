c1d44b92e04281be8398a69075b5e19119edc6db
joshuamotoaki <dachshunduniverse@gmail.com>
2023-09-25 19:47:45 -0400

    Fix toast style

--- src/lib/components/elements/Toast.svelte
+++ src/lib/components/elements/Toast.svelte
@@ -1,33 +1,32 @@
 <script lang="ts">
 import { createEventDispatcher } from "svelte";
 import { fade } from "svelte/transition";
+import infoIcon from "$lib/img/icons/infoicon.svg"
 
 const dispatch = createEventDispatcher();
 
 export let type = "success";
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <article class={type}
 in:fade={{ duration: 150 }}
-out:fade={{ duration: 300 }}>
+out:fade={{ duration: 300 }}
+on:click={() => dispatch("dismiss")}>
     {#if type === "info"}
-        I
+        {infoIcon}
     {:else if type === "error"}
         E
     {:else if type === "warning"}
         W
     {:else}
-        S
+        <img class="w-6" src={infoIcon} alt="Info Icon">
     {/if}
 
     <div class="ml-4">
         <slot />
     </div>
-
-    <button class="close" on:click={() => dispatch("dismiss")}>
-        X
-    </button>
-
 </article>
 
 <style lang="postcss">
@@ -38,13 +37,18 @@ article {
     border-radius: 0.375rem;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    width: 20rem;
+    justify-content: center;
+    width: fit-content;
     margin: 0.5rem auto 0 auto;
+    cursor: pointer;
 }
 
 .success {
     background-color: hsl(120, 52%, 75%);
+}
+
+.success:hover {
+    background-color: hsl(120, 52%, 70%);
 }
 
 .error {
