725064eb40d3920cf0d13eb1ab2cec800156ba9e
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-07 02:57:17 -0400

    Hide timer

--- src/lib/components/recal/modals/RecalUtils.svelte
+++ src/lib/components/recal/modals/RecalUtils.svelte
@@ -35,13 +35,13 @@ export let showModal: boolean = false;
             </svg>              
             Custom Blocks
         </button> -->
-        <button class="btn"
+        <!-- <button class="btn"
         on:click={() => modalStore.open("rtimer", { clear: true })}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>                         
             Timer
-        </button>
+        </button> -->
         <!-- <button class="btn"
         on:click={() => modalStore.open("rstats", { clear: true })}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
--- src/lib/components/recal/modals/SiteTimer.svelte
+++ src/lib/components/recal/modals/SiteTimer.svelte
@@ -9,6 +9,7 @@ let inputFocused: boolean = false;
 
 const siteTimer: Writable<number> = writable(0);
 
+
 const inputToTimerString = (input: number): string => {
     const minutes: number = Math.floor(input / 60);
     const seconds: number = input % 60;
@@ -19,16 +20,8 @@ const inputToTimerString = (input: number): string => {
 
 <StdModal title="Timer" stdClose={true} {showModal}>
     <div slot="main">
-        <!-- Set Timer -->
-        {#key inputFocused}
-        <div on:focus={() => inputFocused = true} 
-            on:blur={() => inputFocused = false}>
-            {#if inputFocused}
-                <input type="text" bind:value={timerInput}>
-            {:else}
-                <h2>{inputToTimerString(timerInput)}</h2>
-            {/if}
-        </div>
-        {/key}
+        <h2>{inputToTimerString(timerInput)}</h2>
+
+        <input type="text" bind:value={timerInput}>
     </div>
 </StdModal>
