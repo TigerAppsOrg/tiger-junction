f025f8097317f7bded2d34e860e1a50a28eae1d4
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-08 23:23:23 -0400

    Fix add button disapearing on overflow

--- src/lib/components/recal/Top.svelte
+++ src/lib/components/recal/Top.svelte
@@ -174,8 +174,10 @@ dark:border-slate-200/60">
                 <button class="termchoice"
                 on:click={() => 
                 modalStore.open("addSchedule", { clear: true })}>
-                    <img src={addIcon} alt="Add Icon"
-                    class="btn-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                    class="h-5 w-5 dark:text-slate-300 text-slate-500">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
                 </button>
             {/key}
         {:catch error}
