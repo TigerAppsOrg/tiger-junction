2a2c8aca3e6a7493c140185a11e70865c329fde6
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-16 08:34:25 -0300

    Boilerplate new functions

---
+++ supabase/functions/redis-transfer/index.ts
@@ -0,0 +1,11 @@
+Deno.serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
