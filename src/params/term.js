7fb7b4670bad58fcdfaec82a0b8f5b3944610c27
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-17 15:28:16 -0300

    Allow term param to match all

--- src/params/term.js
+++ src/params/term.js
@@ -2,5 +2,6 @@ import { TERM_MAP } from "$lib/constants";
 
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
-    return Object.values(TERM_MAP).includes(parseInt(param));
+    return Object.values(TERM_MAP).includes(parseInt(param)) 
+    || param === "all";
 }
