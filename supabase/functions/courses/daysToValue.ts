58a81fcfd5b602904b42279d2bc3f08643b25fb1
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-18 14:15:41 -0400

    Remove unused file

--- supabase/functions/courses/daysToValue.ts
+++
@@ -1,9 +0,0 @@
-export const daysToValue = (section: WeekDays) => {
-  let days = 0;
-  if(section.mon === "Y") days += 1;
-  if(section.tues === "Y") days += 2;
-  if(section.wed === "Y") days += 4;
-  if(section.thurs === "Y") days += 8;
-  if(section.fri === "Y") days += 16;
-  return days;
-};
