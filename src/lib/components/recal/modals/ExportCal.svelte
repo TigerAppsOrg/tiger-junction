c88b2c3b6601cf11475153f6fa3d408e7cd3dd51
joshuamotoaki <dachshunduniverse@gmail.com>
2023-10-21 01:04:14 -0400

    Create export modal UI

--- src/lib/components/recal/modals/ExportCal.svelte
+++ src/lib/components/recal/modals/ExportCal.svelte
@@ -6,13 +6,18 @@ import { currentSchedule, currentTerm } from "$lib/stores/recal";
 import { rMeta } from "$lib/stores/rmeta";
 import { savedCourses } from "$lib/stores/rpool";
 import { sectionData } from "$lib/stores/rsections";
+import { toastStore } from "$lib/stores/toast";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { createEvents, type DateArray, type EventAttributes } from "ics";
-import { onMount } from "svelte";
 
 export let showModal: boolean = false;
 export let supabase: SupabaseClient;
 
+let link: string = "";
+
+/**
+ * Create an ical file and upload it to supabase storage.
+ */
 const createIcal = async () => {
     // Create event for each course
     let events: EventAttributes[] = [];
@@ -65,7 +70,7 @@ const createIcal = async () => {
         }
     }
 
-    createEvents(events, async (error, value) => {
+    await createEvents(events, async (error, value) => {
         if (error) {
             console.log(error);
             return;
@@ -113,6 +118,7 @@ const createIcal = async () => {
             return;
         }
 
+
         // Push to supabase database
         const { error: supabaseError4 } = await supabase
             .from("icals")
@@ -127,15 +133,60 @@ const createIcal = async () => {
             console.log(supabaseError4);
             return;
         }
+
+        link = data.path;
     });
-
 }
-
-onMount(async () => {
-    await createIcal();
-})
 </script>
 
 <StdModal title="Export Schedule" stdClose={true} {showModal}>
+    <div class="flex flex-col space-y-2 md:space-y-4" slot="main">
+        <p class="text-sm">
+            This will create a new iCal file with all your confirmed courses, 
+            which you can add to your calendar app of choice by 
+            downloading the file or using the link.
+            To update your calendar, simply export again.
+            <span class="underline">
+                Note: creating a new export will
+                overwrite any existing 
+                calendar exports.
+            </span>
+        </p>
 
+        {#if link}
+        <div class="flex flex-col md:flex-row items-center justify-between gap-2">
+            <p class="bg-std-gray btn flex-1 text-center break-all text-sm">
+                https://tigerjunction.com/api/client/calendar/{link}
+            </p>
+
+            <div class="flex flex-col sm:flex-row gap-2">
+                <button class="btn bg-std-blue hover:bg-std-darkBlue"
+                on:click={() => {
+                    navigator.clipboard.writeText(`https://tigerjunction.com/api/client/calendar/${link}`);
+                    toastStore.add("success", "Copied link to clipboard!")
+                }}>
+                    Copy Link
+                </button>
+    
+                <button class="btn bg-std-orange hover:bg-std-darkOrange">
+                    <a href={`https://tigerjunction.com/api/client/calendar/${link}`} 
+                    target="_blank" rel="noopener noreferrer">
+                        Download File
+                    </a>
+                </button>
+            </div>
+        </div>
+        {:else}
+            <button class="btn bg-std-green hover:bg-std-darkGreen" 
+            on:click={createIcal}>
+                Export
+            </button>
+        {/if}
+    </div>
 </StdModal>
+
+<style lang="postcss">
+.btn {
+    @apply duration-150 px-4 py-2 rounded-md;
+}
+</style>
