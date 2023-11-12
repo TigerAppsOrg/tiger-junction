8d28137cafc1fafd9ae55957697518da661e2f8e
joshuamotoaki <dachshunduniverse@gmail.com>
2023-11-12 01:26:49 -0500

    Remove utils

--- src/lib/components/recal/Top.svelte
+++ src/lib/components/recal/Top.svelte
@@ -4,10 +4,6 @@ import { currentSchedule, currentTerm, ready, retop, schedules, searchCourseData
 import type { SupabaseClient } from "@supabase/supabase-js";
 
 import addIcon from "$lib/img/icons/addicon.svg";
-import logoutIcon from "$lib/img/icons/logouticon.svg";
-import moonIcon from "$lib/img/icons/moonicon.svg";
-import sunIcon from "$lib/img/icons/sunicon.svg";
-import utilsIcon from "$lib/img/icons/utilsicon.svg";
 import duck from "$lib/img/duck.gif";
 
 import { darkTheme } from "$lib/stores/state";
@@ -132,17 +128,32 @@ dark:border-slate-200/60">
             <button on:click={() => $darkTheme = !$darkTheme}
                 class="btn-circ">
                     {#if $darkTheme}
-                        <img src={moonIcon} alt="Dark Mode Icon" 
-                        class="btn-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                    class="btn-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                    </svg>                      
                     {:else}
-                        <img src={sunIcon} alt="Light Mode Icon" 
-                        class="btn-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                    class="btn-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                    </svg>                      
                     {/if}
+            </button>
+            
+            <button class="btn-circ"
+            on:click={() => modalStore.open("rcolors", { clear: true})}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                class="btn-icon">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />
+                </svg>        
             </button>
 
             <button class="btn-circ"
-            on:click={() => modalStore.open("rutils", { clear: true})}>
-                <img src={utilsIcon} alt="Utils Icon" class="btn-icon">
+            on:click={() => modalStore.open("exportCal", { clear: true})}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                class="btn-icon">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                </svg>       
             </button>
 
             {#if !$isMobile}
@@ -154,14 +165,18 @@ dark:border-slate-200/60">
                 text-sm font-light text-slate-600 dark:text-slate-300"
                 on:click={handleLogout}>
                     Logout
-                    <img src={logoutIcon} alt="Logout Icon"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
                     class="btn-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    </svg>                      
                 </button>
             {:else}
                 <button class="btn-circ"   
                 on:click={handleLogout}>
-                    <img src={logoutIcon} alt="Logout Icon"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
                     class="btn-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    </svg>     
                 </button>
             {/if}
         </div> <!-- * Icon Buttons -->
@@ -211,8 +226,9 @@ dark:border-slate-200/60">
             <button class="card termchoice"
             on:click={() => 
             modalStore.open("addSchedule", { clear: true })}>
-                <img src={addIcon} alt="Add Icon"
-                class="btn-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btn-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>  
             </button>
         {/await}
     {/key}
@@ -263,7 +279,7 @@ dark:border-slate-200/60">
 }
 
 .btn-icon {
-    @apply h-5 w-5 invert-[.5] dark:invert-[.7];
+    @apply h-5 w-5 dark:text-slate-300 text-slate-500;
 }
 
 #duck {
