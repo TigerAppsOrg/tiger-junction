05d95137312799213e5841886a1d46a08dd695ba
Joshua Lau <dachshunduniverse@gmail.com>
2024-01-27 16:48:48 -0500

    Add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
 import type { Config } from "@sveltejs/kit";
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+
+inject({ mode: dev ? "development" : "production" });
 
 export const load = async ({ fetch, data, depends }) => {
     depends("supabase:auth");
