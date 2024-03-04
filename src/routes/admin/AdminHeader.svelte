928f4e87871c4169fc0e28d2d7aca43e943c90fd
Joshua Lau <dachshunduniverse@gmail.com>
2024-03-03 23:57:32 -0500

    Panthera

--- src/routes/admin/AdminHeader.svelte
+++ src/routes/admin/AdminHeader.svelte
@@ -65,7 +65,7 @@ dark:border-zinc-700 border-zinc-200">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
                 </svg>                            
                 <span class="hidden md:block">
-                    ReqTree
+                    Panthera
                 </span>
             </button>
 
