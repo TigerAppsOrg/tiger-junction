b26e0a4403dc4e3f8f0901f249d57cc1d63d006b
joshuamotoaki <dachshunduniverse@gmail.com>
2024-03-11 21:31:42 -1000

    Make linker return boolean

--- prerequisites/scripts/link.js
+++ prerequisites/scripts/link.js
@@ -7,6 +7,9 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 
 /**
+ * Links the courses in the prerequisite files to their respective
+ * course IDs. This script should be run after createResolve.js
+ * to ensure that the cross table is up to date.
  * @returns {boolean} True if successful, false otherwise
  */
 export default function link() {
@@ -29,7 +32,8 @@ export default function link() {
             for (let k = 0; k < courses.length; k++) {
                 if (!crossTable.hasOwnProperty(courses[k].course)) {
                     console.error(`Course ${courses[k].course} not found in cross table`);
-                    process.exit(1);
+                    console.error("Exiting link process");
+                    return false;
                 }
 
                 courses[k].id = crossTable[courses[k].course].id;
@@ -38,6 +42,9 @@ export default function link() {
             fs.writeFileSync(file, "---\n" + yaml.dump(frontMatter) + "---\n" + yaml.dump(courses).trim());
         }
     }
+
+    console.log("Linking complete");
+    return true;
 }
 
 if (process.argv[1].includes("link.js")) {
