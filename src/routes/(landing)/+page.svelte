8baec2bad4345715915cc6dc5fa81762a91e1cd1
Joshua Lau <dachshunduniverse@gmail.com>
2024-05-01 10:40:46 -0400

    Landing page

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
-import { goto } from '$app/navigation';
-import { EMAIL_LIST_FORM_LINK } from '$lib/constants';
-import githubIcon from "$lib/img/github-mark-white.svg";
-
-export let data;
-
-const handleLogin = async () => { 
-    // Redirect if user is already logged in
-    const user = (await data.supabase.auth.getSession()).data.session?.user;
-    if (user) {
-        goto("/recalplus");
-        return;
-    }
-
-    await data.supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-            redirectTo: "https://junction.tigerapps.org/auth/callback"
-        }
-    });
-}
+    import { EMAIL_LIST_FORM_LINK } from '$lib/constants';
+    import { handleLogin } from '$lib/supabase.js';
+    import LandingFooter from './LandingFooter.svelte';
+    import LandingHeader from './LandingHeader.svelte';
+    
+    export let data;
 </script>
-
+    
 <svelte:head>
     <title>TigerJunction</title>
     <meta name="description" content="TigerJunction is a platform for effortless course planning at Princeton University. Experience a seamless blend of modern design and integrated features, simplifying your academic journey. Explore, plan, and succeed.">
@@ -30,175 +15,173 @@ const handleLogin = async () => {
     <meta property="og:image" content="https://junction.tigerapps.org/tjlogo.png" />
     <meta property="og:url" content="https://junction.tigerapps.org/" />
 </svelte:head>
-
-<nav class="flex justify-between items-center text-lg h-20 std-padding">
-    <div id="left">
-        <a href="/" class="flex items-center gap-1">
-            <img src="tjlogonew.png" alt="Tiger Junction logo"
-            class="w-14 h-14">
-            <span class="text-std-darkOrange text2xl">TigerJunction</span>
-        </a>
-    </div>
-    <div id="right" class="space-x-6">
-        <button class="btn btn-green"
-        on:click={handleLogin}>
-            Login
-        </button>
-    </div>
-</nav>
-
-<main class="grid h-[80vh] sm:h-[65vh] py-20 std-padding">
-    <div class="my-auto h-[60vh]">
-        <p class="text-lg mb-2 font-bold">
-            TigerJunction
-        </p>
-        <h1 class="text-5xl font-bold mb-4">
-            <span class="text-std-darkBlue">NEXT</span>
-            <span class="text-std-darkOrange">GENERATION</span>
-            <span class="text-std-darkPink">COURSE</span>
-            <span class="text-std-darkPurple">PLANNING</span>
-        </h1>
-        <p class="mb-6 font-light text-lg">
-            Princeton's Premier Course Planner: Streamline your planning, 
-            explore with ease, and excel effortlessly.
-        </p>
-        <button on:click={handleLogin}
-        class="btn btn-green text-lg space-y-0">
-        <div>
-            Get Started →
-        </div>
-        <div class="font-light text-xs italic">
-            Princeton Email Required
-        </div>
-
-        </button>
-    </div>
-</main>
     
-<div id="about" class="py-20 std-padding">
-    <div class="text-center mb-6">
-        <h1 class="text-5xl font-bold mb-2">
-            Three apps, <span class="text-std-darkRed">one platform</span>
-        </h1>
-        <h2 class="text-lg font-light">    
-            Effortlessly plan your courses with seamless integration
-        </h2>
-    </div>
-    
-    <div class="grid grid-cols-1 lg:grid-cols-3 max-w-6xl gap-6 mx-auto">
-        <div class="card bg-std-pink">
-            <h1>ReCal+</h1>
-            <p class="mb-2">
-                Design and export your ideal schedule with powerful advanced
-                search and filtering capabilities.
-            </p>
-        </div>
 
-        <div class="card bg-std-blue">
-            <h1>CourseGenie</h1>
-            <p class="mb-2">
-                Craft your four-year academic journey with all the
-                information you need at your fingertips.
-            </p>
-        </div>
-
-        <div class="card bg-std-orange">
-            <h1>Panthera</h1>
-            <p class="mb-2">
-                Visualize prerequisites and degree requirements with a 
-                modern, interactive interface.
-            </p>
-        </div>
-    </div>
+<div class="bg-white min-h-screen w-screen flex" id="main">
+    <div class="overlay flex-1 pt-2 flex flex-col items-center">
+        
+        <LandingHeader supabase={data.supabase} />
     
-    <div class="mt-16">
-        <div class="bg-zinc-300 p-8 rounded-lg 
-        text-lg flex justify-between items-center gap-6">
-            <div class="flex items-center gap-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                class="w-10 h-10 hidden md:block">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                </svg>                  
-                Join our emailing list to receive updates about
-                TigerJunction feature releases and other news!
+        <div class="max-w-7xl w-full px-4 sm:px-6 lg:px-8 mt-16">
+            <div class="lg:text-center">
+                <p class="mt-2 text-lg leading-8 font-bold 
+                lg:text-2xl
+                tracking-tight text-gray-900 sm:text-xl">
+                    TigerJunction
+                </p>
+                <h1 class="py-2 max-w-2xl text-4xl sm:text-6xl
+                font-extrabold uppercase
+                lg:mx-auto">
+                    <span class="text-pink-500">
+                        Next
+                    </span>
+                    <span class="text-indigo-500">
+                        Generation
+                    </span>
+                    <span class="text-blue-500">
+                        Course
+                    </span>
+                    <span class="text-purple-500">
+                        Planning
+                    </span>
+                </h1>
+                <p class="mt-2 max-w-2xl text-lg sm:text-xl
+                lg:text-2xl
+                 text-gray-500 lg:mx-auto">
+                    Princeton's premier academic planning platform.
+                    Ultimate customization. Seamless integration.
+                </p>
             </div>
-            <a href={EMAIL_LIST_FORM_LINK} target="_blank">
-                <button class="btn btn-green">
-                    Get Updated
-                </button>
-            </a>
+        
+            
+            <div class="flex gap-4 mt-6 lg:justify-center items-center
+            text-sm sm:text-base">
+                <div class="flex justify-start lg:justify-center">
+                    <button
+                    on:click={() => handleLogin(data.supabase)}
+                    class="bg-indigo-600 hover:bg-indigo-500 text-white 
+                    text-center py-2 px-4 rounded font-bold
+                    active:scale-95 duration-100
+                    ">
+                        Get Started
+                    </button>
+                </div>
+                <div class="flex justify-start lg:justify-center">
+                    <a href={EMAIL_LIST_FORM_LINK} target="_blank"
+                    class="blankbutton">
+                        Join Email List
+                    </a>
+                </div>
+                <!-- <div class="flex justify-start lg:justify-center">
+                    <a href="about" class="blankbutton">
+                        Learn More
+                    </a>
+                </div> -->
+            </div>
         </div>
+        
+        <div class="flex-1"></div>
+        <div id="poweredby" class="glassy max-w-lg
+        w-[90%] mx-auto">
+            <div class="text-gray-500 text-center">
+                Powered By
+            </div>
+            <div class="w-full flex items-center
+            justify-center gap-4 mt-2">
+                <a href="https://svelte.dev/" target="_blank">                
+                    <img src="/home/svelte.png" alt="Svelte"
+                    class="logo">
+                </a>
+                <a href="https://www.typescriptlang.org/" target="_blank">                
+                    <img src="/home/ts.png" alt="Typescript"
+                    class="logo">
+                </a>
+                <a href="https://supabase.com/" target="_blank">
+                    <img src="/home/supabase.png" alt="Supabase"
+                    class="logo">
+                </a>
+                <a href="https://redis.io/" target="_blank">
+                    <img src="/home/redis.svg" alt="Redis"
+                    class="logo">
+                </a>
+                <a href="https://www.anthropic.com/" target="_blank">
+                    <img src="/home/anthropic.jpeg" alt="Anthropic"
+                    class="logo">
+                </a>                
+                <a href="https://tailwindcss.com/" target="_blank">
+                    <img src="/home/tailwind.png" alt="TailwindCSS"
+                    class="logo">
+                </a>
+                <a href="https://melt-ui.com/" target="_blank">
+                    <img src="/home/melt.png" alt="MeltUI"
+                    class="logo">
+                </a>
+                <a href="https://vercel.com/" target="_blank">
+                    <img src="/home/vercel.svg" alt="Vercel"
+                    class="logo">
+                </a>
+                <a href="https://www.cloudflare.com/" target="_blank">
+                    <img src="/home/cloudflare.png" alt="Cloudflare"
+                    class="logo">
+                </a>
+            </div>
+        </div>
+
+        <LandingFooter />
     </div>
-    
 </div>
 
-<footer class="bg-std-darkPurple text-center text-slate-100 
-font-light py-4 text-sm">
-    <div>
-        <span>
-            Made with love with
-            <a href="https://svelte.dev/" target="_blank" class="link">
-                Svelte</a>,
-            <a href="https://supabase.com/" target="_blank" class="link">
-                Supabase</a>,
-            <a href="https://tailwindcss.com/" target="_blank" class="link">
-                TailwindCSS</a>,  
-            <a href="https://melt-ui.com/" target="_blank" class="link">
-                MeltUI</a>, and  
-            <a href="https://pixijs.com/" target="_blank" class="link">
-                PixiJS</a>
-            by Joshua Lau
-        </span>
-    </div>
-    <div>
-        Inspired by <a href="https://recal.io/" target="_blank" class="link">
-            ReCal</a>, 
-            <a href="https://www.tigerpath.io/" target="_blank" class="link">
-                TigerPath</a>,
-            <a href="https://www.princetoncourses.com/" target="_blank" class="link">
-                PrincetonCourses</a>, and
-            <a href="https://tigermap.tigerapps.org/" target="_blank" class="link">
-                TigerMap</a>.
-    </div>
-    <div class="mt-2 flex items-center justify-center gap-2">
-        View the source code on
-        <a href="https://github.com/TigerAppsOrg/TigerJunction" target="_blank">
-            <img src={githubIcon} alt="GitHub icon" class="w-6 h-6 inline-block">
-        </a>
-    </div>
-</footer>
-
 <style lang="postcss">
-.card {
-    @apply p-8 rounded-lg;
-}
+    #main {
+        background-image: radial-gradient(18% 28% at 24% 50%, #FF6B6B00 7%, #FF6B6B00 100%),
+                  radial-gradient(18% 28% at 18% 71%, #FFFFFF9C 6%, #FF6B6B00 100%),
+                  radial-gradient(70% 53% at 36% 76%, #4ECDC4FF 0%, #FF6B6B00 100%),
+                  radial-gradient(42% 53% at 15% 94%, #FFFFFFFF 7%, #FF6B6B00 100%),
+                  radial-gradient(42% 53% at 34% 72%, #FFFFFFFF 7%, #FF6B6B00 100%),
+                  radial-gradient(18% 28% at 35% 87%, #C44EFFCC 7%, #FF6B6B00 100%),
+                  radial-gradient(31% 43% at 7% 98%, #FFFFFFFF 24%, #FF6B6B00 100%),
+                  radial-gradient(21% 37% at 72% 23%, #FFD93DAA 24%, #FF6B6B00 100%),
+                  radial-gradient(35% 56% at 91% 74%, #C44EFFCC 9%, #FF6B6B00 100%),
+                  radial-gradient(74% 86% at 67% 38%, #4ECDC4FF 24%, #FF6B6B00 100%),
+                  linear-gradient(125deg, #FF6B6BCC 1%, #FFFFFFFF 100%);
+        background-size: 200% 200%;
+        animation: gradient 15s ease infinite;
+    }
+  
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
 
-.card h1 {
-    @apply font-bold text-2xl mb-2;
-}
+    .overlay {
+        background-color: rgba(255, 255, 255, 0.4);
+    }
 
-.card p {
-    @apply text-lg font-light;
-}
+    .glassy {
+        background-color: rgba(255, 255, 255, 0.2);
+        backdrop-filter: blur(10px);
+        border-radius: 10px;
+        padding: 20px;
+        margin: 20px;
+        box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.17);
+    }
 
-.btn {
-    @apply rounded-md px-4 py-2;
-}
+    .logo {
+        @apply w-7 h-7 inline-block object-contain;
+    }
 
-.btn:active {
-    @apply transform scale-95;
-}
-
-.std-padding {
-    @apply px-4 sm:px-20;
-}
-
-.btn-green {
-    @apply bg-std-green text-black
-    hover:bg-std-darkGreen duration-150;
-}
-
-.link {
-    @apply underline duration-100 hover:text-zinc-300;
-}
+    .blankbutton {
+        @apply inline-block px-4 py-2 border 
+                    text-center
+                    rounded text-black border-black hover:text-gray-600
+                    hover:border-gray-600
+                    active:scale-95 duration-100
+    }
 </style>
---
+++ src/routes/(landing)/LandingHeader.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { handleLogin } from "$lib/supabase";
+    import type { SupabaseClient } from "@supabase/supabase-js";
+
+    export let supabase: SupabaseClient;
+</script>
+
+<div class="max-w-7xl px-4 sm:px-6 lg:px-8 w-full">
+    <nav class="flex items-center justify-between flex-wrap">
+    <a href="/"
+    class="flex items-center flex-shrink-0 text-black mr-6">
+        <img src="/tjlogonew.png" alt="Tiger Junction logo" class="w-12 h-12">
+        <span class="font-semibold text-xl tracking-tight">TigerJunction</span>
+    </a>
+    <div class="flex-grow flex items-center w-auto">
+        <div class="text-sm flex-grow">
+            <!-- <a href="/about" class="link">
+                About
+            </a>
+            <a href="/developer" class="link">
+                API
+            </a>
+            <a href="/blog" class="link">
+                Blog
+            </a>
+            <a href="/contact" class="link">
+                Contact
+            </a> -->
+        </div>
+        <div>
+            <button on:click={() => handleLogin(supabase)}
+            class="inline-block text-sm px-4 py-2 leading-none border rounded
+             text-black border-black hover:text-gray-600     
+            hover:border-gray-600">
+                Login
+            </button>
+        </div>
+    </div>
+    </nav>
+</div>
+
+<style lang="postcss">
+    .link {
+        @apply inline-block mt-0 text-black hover:text-gray-600 mr-4;
+    }
+</style>
