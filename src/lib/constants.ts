901505a218b17d47d2581275e740962450f5e75f
joshuamotoaki <dachshunduniverse@gmail.com>
2024-05-28 16:25:40 -0700

    Update documentation

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,45 +1,6 @@
 import type { RegGradingInfo } from "./types/regTypes";
 
-// Emailing List Form
-export const EMAIL_LIST_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSebVwd90RtgYf0WtPueOF2BUh8gX2zl-C6Tbjtfxo1E6jo6xA/viewform?usp=sf_link";
-
-// Database
-export const CATEGORIES = ["ab", "bse", "certificate", "minor"];
-
-// Registrar API
-export const TERM_URL = 'https://api.princeton.edu/registrar/course-offerings/classes/';
-export const COURSE_URL = 'https://api.princeton.edu/registrar/course-offerings/1.0.2/course-details?'
-export const EVALUATION_URL = "https://registrarapps.princeton.edu/course-evaluation?";
-
-export const GENERIC_GRADING_INFO: RegGradingInfo = {
-    grading_design_projects: "Design Project", 
-    grading_final_exam: "Final Scheduled Exam",
-    grading_home_final_exam: "Take Home Final Exam",
-    grading_home_mid_exam: "Take Home Midterm Exam", 
-    grading_lab_reports: "Lab Reports",
-    grading_mid_exam: "Midterm Exam",
-    grading_oral_pres: "Presentation or Performance",
-    grading_other: "Other", 
-    grading_other_exam: "Exam(s) Given During Term",
-    grading_paper_final_exam: "Final Paper or Project",
-    grading_paper_mid_exam: "Midterm Paper or Project", 
-    grading_papers: "Papers/Writing Assignments",
-    grading_precept_part: "Participation",
-    grading_prob_sets: "Problem Sets",
-    grading_prog_assign: "Programming Assignments",
-    grading_quizzes: "Quizzes", 
-    grading_term_papers: "Term Paper(s)", 
-    pu_pres_final_exam: "Final presentation or Performance",
-    pu_projects: "Project(s)",
-};
-
-export const STATUS_MAP: Record<string, number> = {
-    "Open": 0,
-    "Closed": 1,
-    "Canceled": 2,
-    "Other": 3,
-}
-
+// Check this for updates every term
 export const DEPARTMENTS = [
     "AAS",
     "AFS",
@@ -154,6 +115,47 @@ export const DEPARTMENTS = [
     "VIS",
     "WRI"
 ]
+
+// Emailing List Form
+export const EMAIL_LIST_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSebVwd90RtgYf0WtPueOF2BUh8gX2zl-C6Tbjtfxo1E6jo6xA/viewform?usp=sf_link";
+
+// Database
+export const CATEGORIES = ["ab", "bse", "certificate", "minor"];
+
+// Registrar API
+export const TERM_URL = 'https://api.princeton.edu/registrar/course-offerings/classes/';
+export const COURSE_URL = 'https://api.princeton.edu/registrar/course-offerings/1.0.2/course-details?'
+export const EVALUATION_URL = "https://registrarapps.princeton.edu/course-evaluation?";
+
+export const GENERIC_GRADING_INFO: RegGradingInfo = {
+    grading_design_projects: "Design Project", 
+    grading_final_exam: "Final Scheduled Exam",
+    grading_home_final_exam: "Take Home Final Exam",
+    grading_home_mid_exam: "Take Home Midterm Exam", 
+    grading_lab_reports: "Lab Reports",
+    grading_mid_exam: "Midterm Exam",
+    grading_oral_pres: "Presentation or Performance",
+    grading_other: "Other", 
+    grading_other_exam: "Exam(s) Given During Term",
+    grading_paper_final_exam: "Final Paper or Project",
+    grading_paper_mid_exam: "Midterm Paper or Project", 
+    grading_papers: "Papers/Writing Assignments",
+    grading_precept_part: "Participation",
+    grading_prob_sets: "Problem Sets",
+    grading_prog_assign: "Programming Assignments",
+    grading_quizzes: "Quizzes", 
+    grading_term_papers: "Term Paper(s)", 
+    pu_pres_final_exam: "Final presentation or Performance",
+    pu_projects: "Project(s)",
+};
+
+export const STATUS_MAP: Record<string, number> = {
+    "Open": 0,
+    "Closed": 1,
+    "Canceled": 2,
+    "Other": 3,
+}
+
 
 // Maximum number of schedules in a term for a user
 export const SCHEDULE_CAP = 10;
