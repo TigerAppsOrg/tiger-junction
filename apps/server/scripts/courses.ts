be7f56c76980246c1f7c2f0dbce75c2da5b0a023
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-22 13:29:15 -0700

    Add fetchers

--- apps/server/scripts/courses.ts
+++ apps/server/scripts/courses.ts
@@ -53,4 +53,69 @@ const populateCourses = async (term: number) => {
   console.log(await fetchRegCourses(term));
 };
 
-populateCourses(1252);
+const fetchDeptCourses = async (dept: string, term: number) => {
+  const token = process.env.API_ACCESS_TOKEN;
+  if (!token) {
+    throw new Error("API access token not found");
+  }
+
+  const rawDeptData = await fetch(
+    `https://api.princeton.edu/student-app/courses/courses?term=${term}&fmt=json&subject=${dept}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+
+  const deptData: any = await rawDeptData.json();
+  if (!deptData.term[0].subjects) {
+    console.error("No courses found for department " + dept);
+    return [];
+  }
+
+  // Find correct department
+  const correctIndex = deptData.term[0].subjects.findIndex(
+    (x: any) => x.code === dept
+  );
+
+  if (correctIndex === -1) {
+    console.error("No courses found for department " + dept);
+    return [];
+  }
+
+  return deptData.term[0].subjects[correctIndex].courses;
+};
+
+const fetchCourseDetails = async (listingId: string, term: number) => {
+  const token = process.env.API_ACCESS_TOKEN;
+  if (!token) {
+    throw new Error("API access token not found");
+  }
+
+  const rawCourseDetails = await fetch(
+    `https://api.princeton.edu/student-app/1.0.3/courses/details?term=${term}&course_id=${listingId}&fmt=json`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+
+  const courseDetails: any = await rawCourseDetails.json();
+
+  const valid =
+    courseDetails &&
+    courseDetails.course_details &&
+    courseDetails.course_details.course_detail;
+
+  if (!valid) {
+    throw new Error("Invalid course details response format");
+  }
+
+  return courseDetails.course_details.course_detail;
+};
+
+console.log(await fetchCourseDetails("017400", 1252));
