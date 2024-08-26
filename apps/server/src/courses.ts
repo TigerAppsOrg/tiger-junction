501bf24407be023acac4e07c05818e69b47b0740
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-26 10:10:28 -0700

    Fetch course information

--- apps/server/src/courses.ts
+++ apps/server/src/courses.ts
@@ -1,9 +1,31 @@
-import { fetchRegListings } from "./fetchers";
+import { fetchRegDeptCourses, fetchRegListings } from "./fetchers";
+import { supabase } from "./shared";
+import type { RegDeptCourses } from "./types";
+
+type ArrayElement<ArrayType extends readonly unknown[]> =
+    ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
 type RegCourse = {
     listing_id: string;
     code: string;
     dists: string[];
+};
+
+type CourseInsert = {
+    id?: number;
+    listing_id: string;
+    title: string;
+    code: string;
+    term: number;
+    dists: string[];
+    status: 0 | 1 | 2;
+    instructors: string[];
+    emplids: string[];
+};
+
+type GradedCourseInsert = CourseInsert & {
+    basis: string;
+    has_final: boolean;
 };
 
 const fetchRegCourses = async (term: number): Promise<RegCourse[]> => {
@@ -21,8 +43,120 @@ const fetchRegCourses = async (term: number): Promise<RegCourse[]> => {
     return regCourses;
 };
 
-const populateCourses = async (term: number) => {
-    console.log(await fetchRegCourses(term));
+const formatStatus = (course: ArrayElement<RegDeptCourses>): 0 | 1 | 2 => {
+    const sections = course.classes;
+
+    let allCanceled = true;
+    const sectionMap: Record<string, boolean> = {};
+
+    for (const section of sections) {
+        const sectionStatus = section.pu_calc_status;
+        const sectionType = section.type_name;
+
+        if (sectionStatus !== "Canceled") allCanceled = false;
+        if (sectionStatus === "Open") sectionMap[sectionType] = true;
+        else if (!sectionMap[sectionType]) sectionMap[sectionType] = false;
+    }
+
+    let status: 0 | 1 | 2 = 0;
+    if (allCanceled) status = 2;
+    else if (Object.values(sectionMap).every(x => x)) status = 0;
+    else status = 1;
+
+    return status;
 };
 
-console.log(await fetchRegCourses(1252));
+const formatInstructors = (
+    course: ArrayElement<RegDeptCourses>
+): [string[], string[]] => {
+    return course.instructors
+        ? [
+              course.instructors.map(x => x.first_name + " " + x.last_name),
+              course.instructors.map(x => x.emplid)
+          ]
+        : [[], []];
+};
+
+export const populateCourses = async (
+    term: number,
+    updateGrading: boolean = false
+) => {
+    let time = new Date();
+    const initPromises = [
+        fetchRegCourses(term),
+        supabase.from("courses").select("id, listing_id").eq("term", term)
+    ];
+
+    const [regCourses, supaRes] = (await Promise.all(initPromises)) as [
+        RegCourse[],
+        {
+            data: {
+                id: number;
+                listing_id: string;
+            }[];
+            error: Error | null;
+        }
+    ];
+
+    if (supaRes.error) {
+        console.error(supaRes.error);
+        return;
+    }
+
+    const supaCourses = supaRes.data;
+    const departments = new Set<string>();
+    regCourses.forEach(x => {
+        departments.add(x.code.substring(0, 3));
+    });
+
+    console.log(
+        "Finished fetching courses in " +
+            (new Date().getTime() - time.getTime()) +
+            "ms"
+    );
+    time = new Date();
+
+    const courseInserts: (CourseInsert | GradedCourseInsert)[] = [];
+    for (const dept of departments) {
+        const deptData = await fetchRegDeptCourses(dept, term);
+        for (const course of deptData) {
+            const regCourse = regCourses.find(
+                x => x.listing_id === course.course_id
+            );
+            if (!regCourse) {
+                console.error(
+                    "Course not found in regCourses:" + course.course_id
+                );
+                continue;
+            }
+
+            const instructorInfo = formatInstructors(course);
+
+            const courseData = {
+                listing_id: course.course_id,
+                title: course.title,
+                code: regCourse.code,
+                term: term,
+                dists: regCourse.dists,
+                status: formatStatus(course),
+                instructors: instructorInfo[0],
+                emplids: instructorInfo[1]
+            } as CourseInsert;
+
+            const supaCourse = supaCourses.find(
+                x => x.listing_id === course.course_id
+            );
+            if (supaCourse) {
+                courseData.id = supaCourse.id;
+            }
+
+            if (updateGrading) {
+                console.log("HO");
+            }
+
+            courseInserts.push(courseData);
+        }
+    }
+};
+
+console.log(await populateCourses(1252));
