7d081e6423306e5a13a8d4f235358c6565419f85
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-26 13:25:05 -0700

    Add section helper functions

--- apps/server/src/courses.ts
+++ apps/server/src/courses.ts
@@ -33,6 +33,20 @@ type CourseInsert = {
     emplids: string[];
 };
 
+type SectionPartial = {
+    course_id?: number;
+    num: string;
+    room: string | null;
+    tot: number;
+    cap: number;
+    days: number;
+    title: string;
+    category: string;
+    start_time: number;
+    end_time: number;
+    status: Status;
+};
+
 type SectionInsert = {
     id?: number;
     course_id: number;
@@ -72,7 +86,7 @@ const fetchRegCourses = async (term: number): Promise<RegCourse[]> => {
     return regCourses;
 };
 
-const formatStatus = (course: ArrayElement<RegDeptCourses>): Status => {
+const formatCourseStatus = (course: ArrayElement<RegDeptCourses>): Status => {
     const sections = course.classes;
 
     let allCanceled = true;
@@ -104,6 +118,68 @@ const formatInstructors = (
               course.instructors.map(x => x.emplid)
           ]
         : [[], []];
+};
+
+const formatRoom = (
+    meeting: ArrayElement<
+        ArrayElement<
+            ArrayElement<RegDeptCourses>["classes"]
+        >["schedule"]["meetings"]
+    >
+): string | null => {
+    if (meeting.building && meeting.room)
+        return meeting.building.name + " " + meeting.room;
+    else return null;
+};
+
+const daysToValue = (days: string[]): number => {
+    let value = 0;
+    if (days.includes("M")) value += 1;
+    if (days.includes("T")) value += 2;
+    if (days.includes("W")) value += 4;
+    if (days.includes("Th")) value += 8;
+    if (days.includes("F")) value += 16;
+    return value;
+};
+
+const timeToValue = (time: string): number => {
+    const TIME_CONVERSION: Record<string, number> = {
+        ZERO_ADJUST: 48,
+        HOUR_FACTOR: 6,
+        MINUTE_FACTOR: 0.1,
+        NULL_TIME: -42
+    };
+
+    if (time === undefined) return TIME_CONVERSION.NULL_TIME;
+
+    const dig = time
+        .split(" ")[0]
+        .split(":")
+        .map(x => parseInt(x));
+    const pm = time.split(" ")[1] === "PM" || time.split(" ")[1] === "pm";
+
+    if (dig[0] === 12) dig[0] = 0;
+
+    let val =
+        dig[0] * TIME_CONVERSION.HOUR_FACTOR +
+        dig[1] * TIME_CONVERSION.MINUTE_FACTOR -
+        TIME_CONVERSION.ZERO_ADJUST;
+
+    if (pm) val += 12 * TIME_CONVERSION.HOUR_FACTOR;
+
+    // Round to nearest tenth (account for floating point error)
+    return Math.round(val * 10) / 10;
+};
+
+const formatSectionStatus = (status: string): Status => {
+    switch (status) {
+        case "Open":
+            return 0;
+        case "Closed":
+            return 1;
+        default:
+            return 2;
+    }
 };
 
 //----------------------------------------------------------------------
@@ -154,6 +230,7 @@ export const populateCourses = async (
     );
     time = new Date();
 
+    const sectionPartials: SectionPartial[] = [];
     const courseInserts: (CourseInsert | GradedCourseInsert)[] = [];
     for (const dept of departments) {
         const deptData = await fetchRegDeptCourses(dept, term);
@@ -168,7 +245,7 @@ export const populateCourses = async (
                 continue;
             }
 
-            // Course Information
+            // Update Course Information
             const instructorInfo = formatInstructors(course);
             const courseData = {
                 listing_id: course.course_id,
@@ -176,7 +253,7 @@ export const populateCourses = async (
                 code: regCourse.code,
                 term: term,
                 dists: regCourse.dists,
-                status: formatStatus(course),
+                status: formatCourseStatus(course),
                 instructors: instructorInfo[0],
                 emplids: instructorInfo[1]
             } as CourseInsert;
@@ -211,9 +288,33 @@ export const populateCourses = async (
                 courseInserts.push(courseData);
             }
 
-            // Section Information
+            // Get Section Information
+            const sections = course.classes;
+            for (const section of sections) {
+                for (const meeting of section.schedule.meetings) {
+                    const sectionData = {
+                        course_id: courseData.id,
+                        num: section.class_number,
+                        room: formatRoom(meeting),
+                        tot: parseInt(section.enrollment),
+                        cap: parseInt(section.capacity),
+                        days: daysToValue(meeting.days),
+                        title: section.section,
+                        category: section.section[0],
+                        start_time: timeToValue(meeting.start_time),
+                        end_time: timeToValue(meeting.end_time),
+                        status: formatSectionStatus(section.pu_calc_status)
+                    } as SectionPartial;
+
+                    sectionPartials.push(sectionData);
+                }
+            }
         }
     }
+
+    // Upload courses to Supabase
+
+    // Handle sections
 };
 
 console.log(await populateCourses(1252, true));
