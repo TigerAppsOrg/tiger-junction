a86ec52f0a33995e72e8486d4e3d692b86cc89bb
Joshua Lau <dachshunduniverse@gmail.com>
2024-10-30 18:11:50 -0400

    Setup drizzle

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+
 export default defineConfig({
     dialect: "postgresql",
-    schema: "./src/db/schema.ts"
+    schema: "./src/db/schema.ts",
+    dbCredentials: {
+        url: process.env.DATABASE_URL
+    },
+    verbose: true,
+    strict: true
 });
