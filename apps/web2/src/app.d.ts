afb9779a2b29e30779e5d23fac73c16c3929cd9c
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-22 11:36:44 -0500

    Scaffold routes

--- apps/web2/src/app.d.ts
+++ apps/web2/src/app.d.ts
@@ -1,10 +1,20 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+import type { Session } from "svelte-kit-cookie-session";
+
+export type SessionData = {
+    netid: string | null;
+    name: string;
+    mail: string;
+};
+
 declare global {
     namespace App {
         // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
+        interface Locals {
+            session: Session<SessionData>;
+        }
+        interface PageData {
+            session: Session<SessionData>;
+        }
         // interface PageState {}
         // interface Platform {}
     }
