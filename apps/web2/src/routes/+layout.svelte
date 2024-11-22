66e1c6307bad5cba161595f53dd0d9e42755ef9c
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-22 12:27:19 -0500

    Constrain width of root layout

--- apps/web2/src/routes/+layout.svelte
+++ apps/web2/src/routes/+layout.svelte
@@ -3,8 +3,9 @@
     import "@fontsource/spectral";
 
     import "../app.css";
-    import { toggleFont } from "$lib/client/font";
     let { children } = $props();
 </script>
 
-{@render children()}
+<div class="max-w-screen overflow-x-hidden">
+    {@render children()}
+</div>
