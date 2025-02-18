1674949e711fe8b204d0877a1741c627f7afbbc2
joshuamotoaki <dachshunduniverse@gmail.com>
2025-02-17 20:10:39 -0500

    Complete oit client

--- apps/engine/lib/engine/clients/oit_client.ex
+++ apps/engine/lib/engine/clients/oit_client.ex
@@ -10,8 +10,37 @@ defmodule Engine.Clients.OitClient do
 
   plug Tesla.Middleware.JSON
 
+  @spec get_terms() :: {:error, any()} | {:ok, any()}
   def get_terms do
     get("/courses/terms", query: [fmt: "json"])
     |> handle_response()
   end
+
+  def get_courses(term, subject, catnum \\ nil, search \\ nil) do
+    query_params =
+      [term: term, subject: subject, fmt: "json"]
+      |> add_if_present(:catnum, catnum)
+      |> add_if_present(:search, search)
+
+    get("/courses/courses", query: query_params)
+    |> handle_response()
+  end
+
+  def get_course_details(term, course_id) do
+    get("/courses/details", query: [term: term, fmt: "json", course_id: course_id])
+    |> handle_response()
+  end
+
+  def get_seats(term, course_ids) do
+    get("/courses/seats", query: [term: term, fmt: "json", course_ids: course_ids])
+    |> handle_response()
+  end
+
+  def get_resseats(term, course_ids) do
+    get("/courses/resseats", query: [term: term, fmt: "json", course_ids: course_ids])
+    |> handle_response()
+  end
+
+  defp add_if_present(params, _key, nil), do: params
+  defp add_if_present(params, key, value), do: Keyword.put(params, key, value)
 end
