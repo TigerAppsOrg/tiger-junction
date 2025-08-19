5747f01f2e15b0c38d5923236b97d1cf220244f6
Joshua Lau <dachshunduniverse@gmail.com>
2025-08-19 10:22:24 -0700

    fix: hotfix icon

--- apps/web/src/lib/components/general/AppHeader.svelte
+++ apps/web/src/lib/components/general/AppHeader.svelte
@@ -108,12 +108,13 @@ dark:border-zinc-700 border-zinc-200"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="size-6">
+                    class="btn-icon">
                     <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
                 </svg>
+
                 {#if !$isMobile}
                     Logout
                 {/if}
